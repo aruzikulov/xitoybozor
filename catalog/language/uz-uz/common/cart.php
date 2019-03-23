bc754d088cc0a1ca1ed2de8f133d1b459e71c58f
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2019-03-24 01:15:44 +0800

    deleted old one

--- catalog/language/uz-uz/common/cart.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Text
-$_['text_items']     = '%s narsa(lar) - %s';
-$_['text_empty']     = "Savatingiz bo'sh";
-$_['text_cart']      = "Savatni ko'rish";
-$_['text_checkout']  = 'Xarid qilish';
-$_['text_recurring'] = "To'lov profili";
--- catalog/language/uz-uz/extension/module/featured.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Mashxur tovarlar';
-
-// Text
-$_['text_tax']      = 'QQS bilan:';
